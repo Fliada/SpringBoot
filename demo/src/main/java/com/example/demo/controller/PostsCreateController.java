ca390688cce6c4cf6775deb15bbe89f5037f8fc4
Alina <wbrflf3301@outlook.com>
2023-02-19 15:04:11 +0500

    метод обработки запроса

--- demo/src/main/java/com/example/demo/controller/PostsCreateController.java
+++ demo/src/main/java/com/example/demo/controller/PostsCreateController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PostsCreateController {
     @RequestMapping(path = "/new", method = RequestMethod.GET)
-    public String list() {
+    public String create() {
         return "create";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String doCreate(@ModelAttribute("text") String text) {
+        return "redirect:/";
     }
 }
